5dd7d608b1793dd2d79fa5a04c93adc525e18b7f
Gelar Rustiawan <gelargew@gmail.com>
2022-12-07 12:17:45 +0700

    slicing

--- index.js
+++ index.js
@@ -1,12 +1,32 @@
 window.addEventListener("DOMContentLoaded", () => {
   const contents = document.querySelectorAll(".contents");
+  const previousContent = document.querySelector(".previousContent");
+  const nextContent = document.querySelector(".nextContent");
+  let selectedContents = null;
+  let offset = 0;
+  const resetState = () => {
+    offset = 0;
+    contents.forEach((_content) => {
+      _content.style.transform = "translateX(0)";
+    });
+    previousContent.classList.add("none");
+  };
+
   const toggler = () => {
-    document.querySelectorAll(".toggler").forEach((el) => {
+    selectedContents = contents[0];
+    const togglers = document.querySelectorAll(".toggler");
+    togglers.forEach((el) => {
       el.addEventListener("click", () => {
+        togglers.forEach((_el) => {
+          _el.classList.remove("active");
+        });
+        el.classList.add("active");
+        resetState();
         const id = el.dataset.listId;
         contents.forEach((content) => {
           if (content.dataset.listId === id) {
             content.classList.remove("none");
+            selectedContents = content;
           } else {
             content.classList.add("none");
           }
@@ -14,6 +34,36 @@ window.addEventListener("DOMContentLoaded", () => {
       });
     });
   };
+  const navigation = () => {
+    nextContent.addEventListener("click", () => {
+      previousContent.classList.remove("none");
+      if (offset + 3 > selectedContents.children.length) {
+        return;
+      } else if (offset + 4 > selectedContents.children.length) {
+        nextContent.classList.add("none");
+      }
+      const width = selectedContents.children[0].getBoundingClientRect().width;
+      selectedContents.style.transform = `translateX(-${
+        width * (offset + 1)
+      }px)`;
+      offset++;
+    });
+    previousContent.addEventListener("click", () => {
+      nextContent.classList.remove("none");
+      console.log(offset);
+      if (!offset) {
+        return;
+      } else if (offset - 1 === 0) {
+        previousContent.classList.add("none");
+      }
+      const width = selectedContents.children[0].getBoundingClientRect().width;
+      selectedContents.style.transform = `translateX(-${
+        width * (offset - 1)
+      }px)`;
+      offset--;
+    });
+  };
 
   toggler();
+  navigation();
 });
